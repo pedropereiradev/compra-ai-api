08baea04d436045bf9d2b67166ee397c428db2aa
Pedro Pereira <pedro.2108@hotmail.com>
2024-10-14 18:45:37 -0300

    feat: error middleware and body payload validation middleware implemented

--- src/core/middlewares/error.middleware.ts
+++ src/core/middlewares/error.middleware.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 import { CustomError } from '../errors/custom-error';
 
 export const errorMiddleware = (err: Error, _req: Request, res: Response) => {
+  console.log({ err, res });
   if (err instanceof CustomError) {
     return res.status(err.status).json({ error: err.message });
   }
--- src/core/middlewares/validate-request.middleware.ts
+++ src/core/middlewares/validate-request.middleware.ts
@@ -3,17 +3,16 @@ import httpStatus from 'http-status';
 import { type AnyZodObject, ZodError } from 'zod';
 
 export const validateRequest = (schema: AnyZodObject) => {
-  async (req: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await schema.parseAsync(req.body);
-
-      return next();
+      next();
     } catch (error) {
       if (error instanceof ZodError) {
         return res.status(httpStatus.BAD_REQUEST).json({ error: error.errors });
       }
-    }
 
-    return next();
+      next();
+    }
   };
 };
