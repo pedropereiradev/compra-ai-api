f1098739be28b2617db3acfbb96e958815b32493
Pedro Pereira <pedro.2108@hotmail.com>
2024-10-14 18:47:07 -0300

    fix: db connection fixed

--- src/core/config/database/data-source.ts
+++ src/core/config/database/data-source.ts
@@ -1,10 +1,22 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import path from 'node:path';
 import dotenv from 'dotenv';
+import { DataSource, type DataSourceOptions } from 'typeorm';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const entitiesDir = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'modules',
+  '**',
+  '*-model.{ts,js}',
+);
+
+const migrationsDir = path.resolve(__dirname, 'migrations', '**', '*.{ts,js}');
+
+export const dbOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number.parseInt(process.env.DB_PORT || '5432', 10),
@@ -13,9 +25,18 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
-  entities: ['src/modules/**/*-model.ts'],
-  migrations: ['src/core/config/database/migrations/**/*.ts'],
-  subscribers: ['src/core/config/database/subscribers/**/*.ts'],
-});
+  entities: [entitiesDir],
+  migrations: [migrationsDir],
+};
 
-// export default AppDataSource;
+export const AppDataSource = new DataSource(dbOptions);
+
+export const initializeDataSource = async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log('[DATABASE]: Database Connection established');
+  } catch (error) {
+    console.error('Error during Data Source initialization', error);
+    throw error;
+  }
+};
