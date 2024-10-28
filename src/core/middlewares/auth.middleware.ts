962808fa2fda8e1632b2c563b886f8e4547e7a14
Pedro Pereira <pedro.2108@hotmail.com>
2024-10-28 20:59:20 -0300

    feat: auth middleware created

---
+++ src/core/middlewares/auth.middleware.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from 'express';
+import { verifyToken } from '../utils/jwt';
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const decoded = verifyToken(token);
+
+  if (!decoded) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
+
+  req.user = decoded;
+  next();
+};
