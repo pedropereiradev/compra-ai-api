68588a2a048be45293b5a16aa9c2da907ac27054
Pedro Pereira <pedro.2108@hotmail.com>
2024-10-28 21:02:03 -0300

    feat: jwt utils created

---
+++ src/core/utils/jwt.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'jwt_secret';
+
+export const generateAccessToken = (userId: number) => {
+  return jwt.sign({ userId }, JWT_SECRET_KEY, { expiresIn: '30m' });
+};
+
+export const generateRefreshToken = (userId: number) => {
+  return jwt.sign({ userId }, JWT_SECRET_KEY, { expiresIn: '30d' });
+};
+
+export const verifyToken = (token: string) => {
+  try {
+    return jwt.verify(token, JWT_SECRET_KEY);
+  } catch (error) {
+    return null;
+  }
+};
